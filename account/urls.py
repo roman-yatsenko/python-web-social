db3fe80bd11c76c2c3c93c3a19c043c4016cb76f
YaRo <roman.yatsenko@hneu.net>
2024-01-25 19:30:42 +0200

    Add password change

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
+    # url-адреси для зміни пароля
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+        
     path('', dashboard, name='dashboard')
 ]
