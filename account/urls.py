1737c74fc068165915fe7b528536f0db5f0cde67
YaRo <roman.yatsenko@hneu.net>
2024-01-25 19:15:04 +0200

    Add auth login/logout pages

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import user_login
-
+from .views import dashboard
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    
+    # url-адреси для входу та виходу
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    path('', dashboard, name='dashboard')
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
 
 from .forms import LoginForm
 
 # Create your views here.
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 def user_login(request):
     if request.method == 'POST':
