254890c5343b646f50f6dbdbdd09828d831b7fe2
YaRo <roman.yatsenko@hneu.net>
2024-02-01 20:05:29 +0200

    Add edit view

--- account/views.py
+++ account/views.py
@@ -3,13 +3,27 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
+from .models import Profile
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.files)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
 
 def register(request):
     if request.method == 'POST':
@@ -22,6 +36,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Зберегти об'єкт User
             new_user.save()
+            # Створити профіль користувача
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
